ce51c23091262da32d9c0fef7b1408647ee77bcb
youlik <tony@gmail.com>
2019-04-24 09:32:25 +0800

    webpack打包vuedemo

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="test">
+     <img src="../assests/logo.png">
+    <p>{{test}}</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test:'Hello Vue.js',
+    }
+  }
+}
+</script>
+
+<style>
+ #test {
+   color: brown;
+   text-align: center
+ }
+</style>
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import App from "./app.vue";
+
+
+new Vue({
+  el:'.container',
+  render:h=>h(App)
+})
